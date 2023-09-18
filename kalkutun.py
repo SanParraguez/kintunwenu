79b1b365b86ab0418f4fc6b5e450fa9460617d71
Santiago Parraguez <sanparra@uni-bremen.de>
2023-09-18 10:51:18 +0200

    Include option to convert units of variables in Kalkutun

--- kalkutun.py
+++ kalkutun.py
@@ -180,6 +180,7 @@ class Kalkutun:
 
                 self.variables.update({
                     'avg_kernel': variables['averaging_kernel'][:],
+                    'std_deviation': variables[product_name+'_precision'][:],
                 })
 
             # -----------------------------------------------
@@ -294,7 +295,7 @@ class Kalkutun:
         return new_object
 
     # -----------------------------------------------------------------------------
-    def convert_units(self, to_unit):
+    def convert_units(self, to_unit, var_list=None):
         """
         Change units of the product data.
 
@@ -302,6 +303,8 @@ class Kalkutun:
         ----------
         to_unit : str
             The desired new unit of the data.
+        var_list : list
+            Which variables to include from the product
 
         Returns
         -------
@@ -309,6 +312,9 @@ class Kalkutun:
         """
         to_unit = standardise_unit_string(to_unit)
         self.data = convert_units(self.data, from_unit=self.units, to_unit=to_unit, species=self.tracer)
+        for var in var_list:
+            self.variables[var] = convert_units(self.variables[var], from_unit=self.units,
+                                                to_unit=to_unit, species=self.tracer)
         self.units = to_unit
         return
 
