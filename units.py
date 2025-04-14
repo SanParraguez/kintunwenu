031fd7602f4e95061470bab779610780ad7c4d9e
Santiago Parraguez <sanparra@uni-bremen.de>
2025-04-14 17:32:55 +0200

    Added coordinates to mapping of unit conversion

--- units.py
+++ units.py
@@ -136,6 +136,10 @@ UNIT_TYPE = {
     'hr'   : 'time',
     'y'    : 'time',
 
+    # Coordinates
+    'degrees_east'  : 'longitudes',
+    'degrees_north' : 'latitudes'
+
 }
 
 # =================================================================================
